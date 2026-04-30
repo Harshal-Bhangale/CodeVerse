6321d697b5dd5f3353e8bd267c9b0acb95c83317
Harshal Bhangale <99475142+Harshal-Bhangale@users.noreply.github.com>
2026-05-01 00:39:10 +0530

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        //  Brute Force
+        // for (int i = 0; i < nums.length; i++) {
+        //     for (int j = i + 1; j < nums.length; j++) {
+        //         if (nums[i] + nums[j] == target) {
+        //             return new int[] {i, j};
+        //         }
+        //     }
+        // }
+        // return new int[]{};
+
+        // HashMap
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[] {map.get(complement), i};
+            }
+
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+
+        // int left = 0;
+        // int right = nums.length - 1;
+
+        // while (left < right) {
+        //     int sum = nums[left] + nums[right];
+
+        //     if (sum == target) {
+        //         return new int[] {left, right};
+        //     } else if (sum < target) {
+        //         left++;
+        //     } else {
+        //         right--;
+        //     }
+        // }
+
+        // return new int[]{};
+    }
+}

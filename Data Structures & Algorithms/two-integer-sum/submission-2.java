079589cc03f687053c0ccf1f6182e0b8ccf7d3f5
Harshal Bhangale <99475142+Harshal-Bhangale@users.noreply.github.com>
2026-05-01 00:13:18 +0530

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        //  Brute Force
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return new int[]{};
+
+        // int left = 0;
+        // int right = nums.length - 1;
+
+        // while (left < right) {
+        //     int sum = nums[left] + nums[right];
+
+        //     if (sum == target) {
+        //         return new int[] {left, right};
+        //     } else if (sum < target) {
+        //         left++;
+        //     } else {
+        //         right--;
+        //     }
+        // }
+
+        // return new int[]{};
+    }
+}

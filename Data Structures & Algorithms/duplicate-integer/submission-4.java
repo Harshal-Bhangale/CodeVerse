96439e5bd6c0e057a79bff731803e12eedcccbeb
Harshal Bhangale <99475142+Harshal-Bhangale@users.noreply.github.com>
2026-05-01 17:18:08 +0530

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        // HashMap<Integer,Integer> map = new HashMap<>();
+
+        // for(int n : nums){
+        //     if(map.containsKey(n)){
+        //         return true;
+        //     }
+
+        //     map.put(n, map.getOrDefault(n,0)+1);
+        // }
+
+        // return false;
+
+        // for(int i = 0; i < nums.length; i++){
+        //     for(int j = i + 1; j < nums.length; j++){
+        //         if(nums[i] == nums[j]){
+        //             return true;
+        //         }
+        //     }
+          
+        // }
+        //   return false;
+
+        // Arrays.sort(nums);
+
+        // for(int i = 1; i < nums.length; i++){
+        //     if(nums[i] == nums[i-1]){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        // Hash Set Length; O(n); O(n)
+        return Arrays.stream(nums).distinct().count() < nums.length;
+    }
+}
